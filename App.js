3b5d7476c73d8b0d354b366638e396cbb3c78d9f
akroise <02022001ak@gmail.com>
2025-10-19 17:22:13 +0530

    Login and config done

--- App.js
+++ App.js
@@ -1,18 +1,44 @@
 import React from "react";
-import { SafeAreaView, StyleSheet } from "react-native";
+import { StyleSheet } from "react-native";
 import { AuthProvider } from "./src/context/AuthContext";
+import { ConfigProvider, useConfig } from "./src/context/ConfigContext";
 import LoginScreen from "./src/screens/LoginScreen";
+
+function AppContent() {
+  const { config, loading, error } = useConfig();
+
+  if (loading) {
+    return (
+      <View>
+        <ActivityIndicator size="large" />
+        <Text>Logging in...</Text>
+      </View>
+    );
+  }
+
+  if (error) {
+    return (
+      <View>
+        <Text>404 Page Not Found</Text>
+      </View>
+    );
+  }
+
+  // ✅ Once config is fetched, render UI
+  return <LoginScreen />;
+
+}
 
 export default function App() {
   return (
     <AuthProvider>
-      <SafeAreaView style={styles.container}>
+      <ConfigProvider>
         <LoginScreen />
-      </SafeAreaView>
+      </ConfigProvider>
     </AuthProvider>
   );
 }
 
-const styles = StyleSheet.create({
-  container: { flex: 1 },
-});
+// const styles = StyleSheet.create({
+//   container: { flex: 1 },
+// });
